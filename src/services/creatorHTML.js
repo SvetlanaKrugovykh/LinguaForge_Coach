d043a224a539f1eee7cc50fdb9db5dee914eefec
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-03-13 13:45:11 +0100

    +

--- src/services/creatorHTML.js
+++ src/services/creatorHTML.js
@@ -1,60 +1,56 @@
 const { selectedByUser } = require('../globalBuffer')
 const fs = require('fs')
-const util = require('util')
 const moment = require('moment')
+const path = require('path')
 const fT = require('../utils/formattedTexts')
 
 module.exports.createHtml = async function (bot, chatId, lang) {
   try {
-    const currentTest = selectedByUser[chatId].currentTest
+    if (!selectedByUser[chatId]) {
+      throw new Error(`No test data found for chatId: ${chatId}`)
+    }
 
+    const currentTest = selectedByUser[chatId].currentTest
     if (!currentTest) {
       throw new Error('No current test found for user')
     }
 
     const HTML_CATALOG = process.env.TEMP_DOWNLOADS_CATALOG
-    const filePath = `${HTML_CATALOG}${chatId}.html`
+    const filePath = path.join(HTML_CATALOG, `${chatId}.html`)
     const content = []
 
-    moment.updateLocale(lang, {
-      week: {
-        dow: 1,
-        doy: 7
-      }
-    })
+    moment.updateLocale(lang, { week: { dow: 1, doy: 7 } })
 
-    content.push(
-      { text: `${currentTest?.topic || 'test'}`, style: 'header', fontSize: '18px' },
-    )
+    content.push({ text: currentTest?.topic || 'Test', style: 'header', fontSize: '18px' })
 
     let { formattedText, formattedOptions } = fT.formatTextAndOptions(currentTest.text, currentTest.options)
-    // formattedText = formattedText.replace(/\n/g, '<br>')
     formattedOptions = formattedOptions.replace(/\n/g, '<br>')
 
-    content.push({
-      text: formattedText,
-      style: 'defaultStyle',
-      fontSize: '16px',
-      marginLeft: '20px'
-    })
+    const formattedTextHTML = formatTextHTML(formattedText)
+    content.push({ text: formattedTextHTML })
 
     const formattedOptionsHTML = formatOptionsHTML(formattedOptions)
-    content.push({
-      text: formattedOptionsHTML,
-      style: 'defaultStyle',
-      fontSize: '16px'
-    })
+    content.push({ html: formattedOptionsHTML })
 
     const htmlContent = createHtmlContent(content)
     await writeHtmlToFile(htmlContent, filePath)
-    await bot.sendDocument(chatId, filePath)
-    fs.unlink(filePath, () => console.log(`File ${filePath} deleted`))
 
+    await bot.sendDocument(chatId, filePath)
+    await fs.promises.unlink(filePath)
+
+    console.log(`File ${filePath} deleted`)
     return true
   } catch (error) {
-    console.error('Error in function createHtml:', error)
+    console.error('Error in createHtml:', error)
     return null
   }
+}
+
+function formatTextHTML(text) {
+  const questionRegex = /(\d{1,3}\.\s)/g
+  let formattedText = text.replace(questionRegex, match => `<span style="font-weight: bold; color: blue;">${match}</span>`).replaceAll('[_', '[_______________')
+  formattedText = `<p style="font-size: 16px; margin-left: 20px; text-align: justify;">&nbsp;&nbsp;&nbsp;     ${formattedText}</p>`
+  return formattedText
 }
 
 function formatOptionsHTML(options) {
@@ -63,33 +59,35 @@ function formatOptionsHTML(options) {
   const questionNumbers = options.match(questionRegex) || []
 
   let formattedOptions = ''
+
   questions.forEach((question, index) => {
-    formattedOptions += `<p style="margin-left: 20px;"><span style="color: blue;">${questionNumbers[index] || `${index + 1}.`}</span> ${question.trim()}</p>`
+    formattedOptions += `<p style="margin-left: 20px; font-size: 16px; font-weight: normal;">
+      ${questionNumbers[index] || `${index + 1}.`}</><br>${question.trim()}
+    </p>`
   })
 
-  const optionRegex = /([a-z]\)\s)/g
-  formattedOptions = formattedOptions.replace(optionRegex, match => `<br>&nbsp;&nbsp;&nbsp;&nbsp;<span style="color: green;">${match}</span>`)
-
-  return formattedOptions
+  return formattedOptions.replace(/([a-z]\)\s)/g, match =>
+    `<span style="color: blue;">${match}</span>`
+  )
 }
 
+
 function createHtmlContent(content) {
-  let htmlContent = '<html><head>'
-  htmlContent += '<meta charset="utf-8">'
-  htmlContent += '<meta name="viewport" content="width=device-width, initial-scale=1">'
-  htmlContent += '<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">'
-  htmlContent += '<style>'
-  htmlContent += 'body { font-family: "Roboto", sans-serif; margin: 20px; }'
-  htmlContent += 'h1 { font-weight: bold; font-size: 18px; text-align: center; }'
-  htmlContent += 'p { font-size: 14px; }'
-  htmlContent += '</style>'
-  htmlContent += '</head><body>'
+  let htmlContent = `
+    <html><head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+    </head><body style="font-family: 'Roboto', sans-serif; margin: 20px;">
+  `
 
   for (const item of content) {
-    if (item.style === 'header') {
-      htmlContent += `<h1>${item.text}</h1>`
+    if (item.html) {
+      htmlContent += item.html
     } else {
-      htmlContent += `<p style="margin-left: ${item.marginLeft || '0px'}; font-size: ${item.fontSize};">${item.text}</p>`
+      htmlContent += item.style === 'header'
+        ? `<h1 style="font-weight: bold; font-size: 18px; text-align: center;">${item.text}</h1>`
+        : `<p style="font-size: 16px; margin-left: 20px;">${item.text}</p>`
     }
   }
 
@@ -97,9 +95,10 @@ function createHtmlContent(content) {
   return htmlContent
 }
 
+
 async function writeHtmlToFile(htmlContent, filePath) {
   try {
-    await util.promisify(fs.writeFile)(filePath, htmlContent, 'utf-8')
+    await fs.promises.writeFile(filePath, htmlContent, 'utf-8')
     console.log(`HTML successfully written to ${filePath}`)
   } catch (error) {
     console.error('Error writing HTML to file:', error)
