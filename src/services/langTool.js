3e157243bdc5c8d54bf257d626aeead5aea6e54e
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-11-25 22:15:12 +0100

    +++

--- src/services/langTool.js
+++ src/services/langTool.js
@@ -1,40 +1,60 @@
 const axios = require('axios')
 const qs = require('qs')
 const { inputLineScene } = require('../controllers/inputLine')
+const { selectedByUser } = require('../globalBuffer')
+const prompts = require('../data/prompts')
+require('dotenv').config()
 
 module.exports.check = async function (bot, msg, lang) {
   try {
-    const URL_LANG_TOOL = process.env.URL_LANG_TOOL
-
     let inputLength = 3
     const text = (await inputLineScene(bot, msg)).trim()
+    const voiceSynthesisLanguage = selectedByUser[msg?.chat?.id]?.voiceSynthesisLanguage || null
+
+    if (!voiceSynthesisLanguage) return
 
     if (!text || text.length < inputLength) {
       await bot.sendMessage(msg.chat.id, 'that`s not enough\n', { parse_mode: 'HTML' })
       return
     }
 
-    const data = qs.stringify({
-      language: lang,
-      text: text
+    const response = await ollamaRequest('language', {
+      text: text,
+      language: voiceSynthesisLanguage,
+      native_language: lang
     })
 
-    const response = await axios.post(URL_LANG_TOOL, data, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-
-    if (response.status === 200) {
-      const matches = response.data.matches
-      const message = formatMatches(matches)
-      await bot.sendMessage(msg.chat.id, message, { parse_mode: 'HTML' })
-    } else {
-      console.error('Error response when check LANG_TOOL:', response.statusText)
-    }
+    await bot.sendMessage(msg.chat.id, response, { parse_mode: 'HTML' })
   } catch (error) {
     console.error('Error check LANG_TOOL:', error.message)
   }
+}
+
+async function ollamaRequest(promptName, promptParams) {
+  const URL_LANG_TOOL = process.env.URL_LANG_TOOL
+  const promptText = prompts[promptName](promptParams)
+  const response = await axios.post(
+    URL_LANG_TOOL,
+    {
+      model: 'qwen2:7b',
+      prompt: promptText
+    },
+    { headers: { 'Content-Type': 'application/json' } }
+  )
+  let resultText = ''
+  if (typeof response.data === 'string') {
+    response.data.split('\n').forEach(line => {
+      try {
+        const obj = JSON.parse(line)
+        if (obj.response) resultText += obj.response
+      } catch (e) { }
+    })
+  } else if (response.data.response) {
+    resultText = response.data.response
+  }
+
+  return resultText.trim()
+
 }
 
 function formatMatches(matches) {
